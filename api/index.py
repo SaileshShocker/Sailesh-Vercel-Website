f86821e921033756f95fd78815333b9a7c952c7e
SaileshSivan <sailesh.shocker@gmail.com>
2025-08-15 22:07:41 +0530

    Fixed image loading issue.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from app import create_app
 from datetime import datetime
 import os
+from flask import send_from_directory
 
 # Set environment for Vercel
 os.environ['FLASK_ENV'] = 'production'
@@ -16,6 +17,11 @@ try:
     @app.route('/api/test')
     def test():
         return {'message': 'Flask app is working on Vercel!'}
+
+    # Custom static file handler for Vercel
+    @app.route('/static/<path:filename>')
+    def custom_static(filename):
+        return send_from_directory('app/static', filename)
 
     # This is the entry point for Vercel
     app.debug = False
